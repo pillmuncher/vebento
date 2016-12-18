170cb92f5320e3516363e1bcdbf590da592fb875
Mick Krippendorf <pillmuncher@users.noreply.github.com>
2016-12-19 00:04:56 +0100

    removed commented code leftover from experimenting.

--- src/componad.clj
+++ src/componad.clj
@@ -31,28 +31,6 @@
     (rws/run-rws-t error-rws computation nil env)))
 
 
-;(defmacro mdo-future-body
-  ;[sys computations]
-  ;`(try
-     ;(either #(vector :failure %)
-             ;#(vector :success %)
-             ;(fst (within (system ~sys) ~@computations)))
-     ;(catch Exception e#
-       ;(vector :error e#))))
-
-
-;(defmacro mdo-future
-  ;[& computations]
-  ;`(>>= ask
-        ;#(let [f# (future (mdo-future-body % ~computations))]
-           ;(return (reify clojure.lang.IDeref
-                     ;(deref [ref]
-                       ;(let [[state# result#] @f#]
-                         ;(case state#
-                              ;:success (return result#)
-                              ;:failure (fail result#)
-                              ;:error (throw result#)))))))))
-
 (defn >>=
   [m & mfs]
   (reduce monad/>>= m mfs))
